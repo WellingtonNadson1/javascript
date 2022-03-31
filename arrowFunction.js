1e0f3fbc494eb5946ca5b56418e006b3e0b27867
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-03-31 19:01:22 -0300

    :construction: I answer the challenge.
    #

--- arrowFunction.js
+++ arrowFunction.js
@@ -11,6 +11,11 @@ const arrow1 = () => {`I am an arrow1 function.`} // I have to take off the { }
 
 const arrow2 = () => `I am an arrow2 function.`
 
+const AboutMe = (() => {
+    console.log(`I find it a little difficult to talk about myself. But one thing is for sure, 
+  I like technology and learning something every day that can make me evolve.`)
+  })();
+
 console.log(anonymous());
 console.log(arrow());
 console.log(arrow1());
--- challenge1.js
+++ challenge1.js
@@ -1,15 +1,30 @@
 // Create a range function
-// range(5) -> [1, 2, 3, 4, 5]
-// range(6, 11) -> [6, 7, 8, 9, 10, 11]
-// range(10, 19, 2) -> [10, 12, 14, 16, 18]
-// range(6, 2) -> [6, 5, 4, 3, 2]
-// range (8, -3, 4) -> [8, 4, 0]
+function range(a, b, s = 1){
+    const n1 = b === undefined ? 1 : a;
+    const n2 = b === undefined ? a : b;
+    const step = n1 <= n2 ? Math.abs(s): -Math.abs(s);
 
-function range(...n){
-    for (let i = 0; i < n.length; i++) {
-        
-        console.log(element)
+    let arr = [];
+    for (let i = n1; n1 <= n2 ? i <= n2 : i >= n2; i += step) {
+        arr.push(i);
     }
+    console.log(arr);
 }
 
+// range(5) -> [1, 2, 3, 4, 5]
 range(5);
+
+// range(6, 11) -> [6, 7, 8, 9, 10, 11]
+range(6, 11);
+
+// range(10, 19, 2) -> [10, 12, 14, 16, 18]
+range(10, 19, 2);
+
+// range(6, 2) -> [6, 5, 4, 3, 2]
+range(6, 2);
+
+// range (8, -3, 4) -> [8, 4, 0]
+range (8, -3, 4);
+
+
+
