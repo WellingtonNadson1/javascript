64890ee9f5395a8a5b53366df7870c1512e5b608
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-04-09 21:47:09 -0300

    :construction: Function return other function
    #

---
+++ challenge2.js
@@ -0,0 +1,41 @@
+//Function return other function
+function somar(n1){
+    return function (n2){
+        return function (n3){
+            return n1 + n2 + n3
+        }
+    }
+}
+const test = somar(3)(4)(5);
+const soma = somar(1);
+const plus = soma(2);
+const resultSoma = plus(3);
+
+console.log(resultSoma, test);
+
+
+function calcular(n1){
+    return function (n2){
+        return function (fn){
+            return fn(n1, n2)
+        }
+    }
+}
+
+function subtrair (a, b){
+    return a - b
+}
+
+function adicionar (a, b){
+    return a + b
+}
+
+function dividir (a, b){
+    return a / b
+}
+
+function multiplicar (a, b){
+    return a * b
+}
+const r1 = calcular(3)(7)(subtrair);
+console.log(r1);
